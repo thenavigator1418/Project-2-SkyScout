58c906925a138a3e646f927a47b9ff2569165f80
Dija <145768761+dija-64@users.noreply.github.com>
2024-01-17 18:57:08 +0000

    Added components folder and footer folder

---
+++ skyscout/src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="bottom">
+
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
