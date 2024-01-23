df5e7d65e18cff1abeb7f09a3a51fa07dbf3f694
Dija <145768761+dija-64@users.noreply.github.com>
2024-01-23 18:55:06 +0000

    resolving conflicts

--- skyscout/src/components/Footer/footer.jsx
+++ skyscout/src/components/Footer/footer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./footer.css";
 import googlemap from "../Map/googlemap";
-<<<<<<< HEAD
-import form from "../ContactForm/form"
-=======
 import ContactForm from "../ContactForm/form";
->>>>>>> e914a54f3a0aa187eb52cd58aacaaefe93080998
 
 const Footer = () => {
     return (
