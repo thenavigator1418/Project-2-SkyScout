7821c73322ada5ba0744757014c4910a3fed05ef
Dija <145768761+dija-64@users.noreply.github.com>
2024-01-23 18:15:53 +0000

    File typo

--- skyscout/src/components/Footer/footer.jsx
+++ skyscout/src/components/Footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 import googlemap from "../Map/googlemap";
-import form from "../ContactForm"
+import form from "../ContactForm/form"
 
 const Footer = () => {
     return (
