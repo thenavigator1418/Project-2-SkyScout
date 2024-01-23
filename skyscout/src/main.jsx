d6bb6345ee4db9fdffc559f503150ee1901b71a7
thenavigator1418 <henrydavis95@hotmail.co.uk>
2024-01-23 17:24:44 +0000

    debugging gallery

--- skyscout/src/main.jsx
+++ skyscout/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar/Navbar.jsx'
+import Gallery from './components/ImgGallery/ImgGallery.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
     <Navbar />
+    <Gallery />
   </React.Fragment>,
 )
