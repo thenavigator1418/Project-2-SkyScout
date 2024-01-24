51e88d4c811843c48f97d0db4771fcd234b8f1c0
thenavigator1418 <henrydavis95@hotmail.co.uk>
2024-01-24 20:53:41 +0000

    Final component assembly

--- skyscout/src/main.jsx
+++ skyscout/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< Updated upstream
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // React components
@@ -8,22 +7,15 @@ import Navbar from './components/Navbar/Navbar.jsx'
 import Body from './components/Body/body.jsx'
 import MyCarousel from './components/ImgGallery/ImgGallery.jsx'
 import Footer from './components/Footer/footer.jsx'
-=======
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.css';
 import Hero from './components/Hero/'
->>>>>>> Stashed changes
 
 // Assembly of React components
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
-<<<<<<< Updated upstream
     <Navbar />
+    <Hero />
     <Body />
     <MyCarousel />
     <Footer />
-=======
-    <Hero />
->>>>>>> Stashed changes
   </React.Fragment>,
 )
